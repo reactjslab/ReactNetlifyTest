d0ef2101e29ab9f71b7b70990397094913f374c8
ubaidh <ubaidh@live.com>
2020-10-25 02:55:22 +0530

    navbrand color mild

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Footer.css'
+
 class Footer extends React.Component {
     render() {
         return (
             <div className="footermain">
-                
+            
             </div>
         )
     }
