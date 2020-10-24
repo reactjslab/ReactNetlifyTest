f87a16a922df888e3c083346bc96b28e6e9679a6
ubaidh <ubaidh@live.com>
2020-10-24 20:00:07 +0530

    first commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Brand from './Brand';
+import './Header.css'
+class Header extends Component {
+  render() {
+    return (
+     
+        <Brand/>
+      
+    )
+  }
+}
+
+export default Header;
