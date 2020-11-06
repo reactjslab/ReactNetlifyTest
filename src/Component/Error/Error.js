7f31168a7efcadff229a7d6de64ff1d29f70b5db
ubaidh <ubaidh@live.com>
2020-11-06 18:49:37 +0530

    pages

---
+++ src/Component/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './../Header/Header'
+import Footer from './../Footer/Footer'
+export default function Error() {
+    return (
+        <>
+        <Header/>
+        <h1>Error</h1>
+        <Footer/>
+        </>
+    )
+}
