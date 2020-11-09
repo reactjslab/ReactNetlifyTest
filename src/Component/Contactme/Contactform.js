c670aceed3523228b318e9a89bdbc0f31c492e82
ubaidh <ubaidh@live.com>
2020-11-10 01:28:09 +0530

    contact form '

---
+++ src/Component/Contactme/Contactform.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Contactform.css'
+function Contactform() {
+    const nameref=React.useRef(null)
+    const emailref=React.useRef(null)
+    const messageref=React.useRef(null)
+    const [formdata,formfunc]=React.useState({name:'',email:'',message:''})
+    const [flip,flipfun] = React.useState(false)
+    function thankyou(e){
+        e.preventDefault()
+        if(nameref.current.value==='' & nameref.current.value==='' & nameref.current.value==='') 
+            console.log("empty")
+        else{
+            formfunc({name:nameref.current.value,email:emailref.current.value, message:messageref.current.value})
+            
+            flipfun(true)
+            console.log(formdata)
+        }
+    }
+    React.useEffect(function () {
+        if(!flip)
+            nameref.current.focus()
+        })
+    return (
+        <div className="formdiv">
+            {flip?<h1>submitted</h1>:
+            <form className="cform" >
+                
+                <label className="labelup" htmlFor="name">Name</label>
+                <input ref={nameref} className="" placeholder="" id="name" type="text" required/>
+                <label className="labelup" htmlFor="email">Email</label>
+                <input ref={emailref}  placeholder="" id="email" type="email" required/>
+                <label className="labelup" htmlFor="message">Message</label>
+                <textarea ref={messageref}  id="message" type="text" rows="10" required></textarea>
+                <button onClick={thankyou} type="submit" className="btn btn-lg btn-primary" >send</button>
+            </form>
+        } 
+        </div>
+    )
+}
+export default Contactform
--- src/Component/Header/Brand.js
+++ src/Component/Header/Brand.js
@@ -9,10 +9,10 @@ const Brand=() =>{
       <h6 className="my-0 mr-md-auto navBrand">
         <Link to="/" className="nounder">chandra marazzi</Link></h6>
       <nav className="my-2 my-md-0 mr-md-3">
-      <NavLink className="navel p-3 text-dark active" exact to ="/" >Home</NavLink>
-      <NavLink className="navel p-3 text-dark" to="/contact">Contact</NavLink>
+      <NavLink className="navel p-3 text-dark" exact to ="/" >Home</NavLink>
       <NavLink className="navel p-3 text-dark" to="/about">About</NavLink>
-       </nav>
+      <NavLink className="navel p-3 text-dark" to="/contact">Contact</NavLink>
+      </nav>
      
     </div>
   )
