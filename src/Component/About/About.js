b62942982a5e7e674917b30589ede5e9d69552fe
ubaidh <ubaidh@live.com>
2020-11-11 23:22:13 +0530

    footer

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './../Header/Header'
 import Footer from './../Footer/Footer'
+import Aboutme from'./Aboutme'
 export default function About() {
     return (
         <>
         <Header/>
+        <Aboutme/>
         <Footer/>
         </>
     )
---
+++ src/Component/About/Aboutme.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './About.css'
+const Aboutme=()=>{
+    return(
+        <div className="aboutdiv">
+            
+        </div>
+    )
+}
+export default Aboutme
