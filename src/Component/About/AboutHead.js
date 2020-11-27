53703752432b07204fabb70a5495fb9ec025efa3
ubaidh <ubaidh@live.com>
2020-11-28 02:52:22 +0530

    final:

--- src/Component/About/AboutHead.js
+++ src/Component/About/AboutHead.js
@@ -14,13 +14,13 @@ export default function AboutHead() {
                 <div className="descdetail col-md">
                     <div className="nametitle d-flex align-items-center flex-column ">ni made candra devi</div>
                     <div className="bodydesc mb-3">
-                        Hello there i am candra devi i am from Bali Indonesia 
+                        Hello there i am candra devi. i am from Bali Indonesia.
                         i am a nurse by profession and a Japanese Language Teacher 
                         based in bali .i teach japanese Language to Nurseing students at Hurry 
                         Japnese Language school and also i teach private classes to people who 
                         are interested in working in japan or just learning japanese language.
                         I have a Diploma in japanese Language and culture from JAPAN FOUNDATION JAKARTA
-                        in Jakarata Indonesia and ASSOCIATION FOR OVERSEAS TECHNICAL CORPORATION AND SUSTAINIBLE 
+                        in Jakarat Indonesia and ASSOCIATION FOR OVERSEAS TECHNICAL CORPORATION AND SUSTAINIBLE 
                         PATERNERSHIP and  JAPAN INTERNATIONAL CORPORATION OF WELFARE SERVICE in Toyota City Aichi Perfecture Japan.
                         I have spent  four and half years in japan and i love japan ,its culture and language.
                         <br/>
